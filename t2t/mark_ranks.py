db9e5510df2279a4c084639fe566f0c1af5e0ead
dparks1134 <donovan.parks@gmail.com>
2015-01-23 13:13:47 +1000

    First working implementation.

--- t2t/mark_ranks.py
+++ t2t/mark_ranks.py
@@ -50,7 +50,34 @@ class MarkRanks(object):
 
         return sum_dist / num_tips
 
-    def mark(self, tree, start_labels, start_rank, output):
+    def _remove_dummy_nodes(self, node):
+        """Remove all dummy nodes from a tree.
+
+        A dummy node is define as a node with only one child.
+
+        This function modifies the tree specified by node.
+
+        Parameters
+        ----------
+        node : TreeNode
+            Internal node of interest.
+        """
+
+        # get list of nodes in preorder as we will be
+        # modifying the tree as we go
+        preorder = list(node.preorder())
+
+        for n in preorder:
+            if len(n.children) == 1:
+                # remove dummy node and connect its single
+                # child to its parent; adjusting the branch
+                # length as necessary
+                length = n.length + n.children[0].length
+                n.children[0].length = length
+                n.parent.append(n.children[0])
+                n.parent.remove(n)
+
+    def mark(self, tree, start_labels, start_rank, verbose=False):
         """Determine taxonomic ranks for all internal nodes.
 
         Each internal node is assigned a predicted taxonomic
@@ -61,12 +88,10 @@ class MarkRanks(object):
         ----------
         tree : str or TreeNode
             A newick string or a TreeNode
-        start_label: str
-            Label of node to start marking internal nodes.
+        start_labels: str
+            Labels of nodes to start marking taxonomic ranks.
         start_rank: int
             Rank of node with start_label.
-        ouput_tree : Output stream
-            Name of tree with marked taxonomic ranks.
         """
 
         # make sure we have a TreeNode object
@@ -78,60 +103,110 @@ class MarkRanks(object):
         start_nodes = []
         try:
             for label in start_labels:
-                start_nodes.append(root.find(label))
+                start_node = root.find(label)
+                start_node.name = self.rank_prefixes[start_rank] + '|' + start_node.name
+                start_nodes.append(start_node)
         except:
             print 'Unable to locate node with label: ' + label
-            return
+            return None
 
-        cur_nodes = set(start_nodes)
-        for cur_rank in xrange(start_rank + 1, len(self.rank_prefixes) - 1):
-            # mark putative nodes at current rank
-            nodes_at_next_rank = set()
-            for node in cur_nodes:
-                # get average distance to ancestral nodes
-                # marked at parental rank
+        nodes_at_rank = set(start_nodes)
+
+        # assign internal nodes with ranks from [phylum, genus],
+        # as it is assumed the terminal taxa are dereplicated at
+        # the species-level
+        for cur_rank in xrange(start_rank, len(self.rank_prefixes) - 1):
+            print '******************* RANK: ' + str(cur_rank)
+
+            # get immediate children of nodes at current rank
+            child_lineages = set()
+            for n in nodes_at_rank:
+                for child in n.children:
+                    child_lineages.add(child)
+
+            child_lineages = child_lineages - nodes_at_rank
+
+            # mark putative nodes at child rank
+            next_nodes = set()
+            for node in child_lineages:
+                print 'processing node: ' + node.name
+
+                # get average distance to ancestral nodes marked at current rank
                 parent_node = node.parent
                 dist = node.length
-                dist_to_phyla = []
+                dist_to_parent_rank = []
                 while parent_node != None and node not in start_nodes:
-                    if parent_node.name.startswith(self.rank_prefixes[cur_rank - 1]):
-                        dist_to_phyla.append(dist)
+                    if parent_node.name.startswith(self.rank_prefixes[cur_rank]):
+                        dist_to_parent_rank.append(dist)
 
                     if parent_node.length:
                         dist += parent_node.length
                     parent_node = parent_node.parent
 
-                if len(dist_to_phyla) != 0:
-                    mean_dist_to_phyla = sum(dist_to_phyla) / len(dist_to_phyla)
+                if len(dist_to_parent_rank) != 0:
+                    mean_dist_to_parent_rank = sum(dist_to_parent_rank) / len(dist_to_parent_rank)
                 else:
-                    mean_dist_to_phyla = 0
+                    mean_dist_to_parent_rank = 0
+
+                print '  mean_dist_to_parent_rank: ' + str(mean_dist_to_parent_rank)
 
                 # get average distance to leaf nodes
                 average_dist = self.average_dist_to_leaves(node)
+                print '  average_dist: ' + str(average_dist)
 
-                # get distance from node to rank of class
-                rank_dist = (mean_dist_to_phyla + average_dist) / (len(self.rank_prefixes) - cur_rank - 1)
+                # get distance from current node for descendant nodes
+                # to be marked at the current rank
+                rank_dist = (mean_dist_to_parent_rank + average_dist) / (len(self.rank_prefixes) - cur_rank - 1)
+                print '  rank_dist: ' + str(rank_dist)
 
-                # mark putative nodes at the current rank and get children
-                # nodes below this rank
-                nodes_at_rank = set()
-                children_nodes = set()
-                for n in node.non_tips():
-                    if n.accumulate_to_ancestor(node) + mean_dist_to_phyla < rank_dist:
-                        n.name = self.rank_prefixes[cur_rank] + '|' + n.name
-                        print self.rank_prefixes[cur_rank] + ': ' + n.name
+                # mark putative nodes at the child rank
+                nodes_to_process = [node]
+                nodes_at_child_rank = set()
+                while nodes_to_process:
+                    n = nodes_to_process.pop()
 
-                        nodes_at_rank.add(n)
+                    if n == node:
+                        dist = mean_dist_to_parent_rank
+                    else:
+                        dist = n.accumulate_to_ancestor(node) + mean_dist_to_parent_rank
+
+                    if dist <= rank_dist:
+                        n.name = self.rank_prefixes[cur_rank + 1] + '|' + n.name
+
+                        if verbose:
+                            print '  ' + self.rank_prefixes[cur_rank + 1] + ': ' + n.name
+
+                        nodes_at_child_rank.add(n)
                         for child in n.children:
-                            children_nodes.add(child)
+                            nodes_to_process.append(child)
 
-                # the children of a node marked at the current rank
-                # may also be marked at the current rank so should be
-                # removed from the set of putative nodes to be marked
-                # at the next rank
-                nodes_at_next_rank |= (children_nodes - nodes_at_rank)
+                # add in dummy nodes so all children lineages contain a
+                # node at the child rank
+                if not nodes_at_child_rank:
+                    name = self.rank_prefixes[cur_rank + 1] + '|' + node.name
+                    parent = node.parent
+                    dummy = TreeNode(name, 0.5 * rank_dist, parent, [node])
+                    parent.append(dummy)
+                    node.length = node.length - 0.5 * rank_dist
 
-            cur_nodes = nodes_at_next_rank
+                    nodes_at_child_rank.add(dummy)
+
+                    print '  name of dummy node: ' + dummy.name
+                    print '  children of dummy node: ' + ','.join([x.name for x in dummy.children])
+                    print '  parent of dummy node: ' + dummy.parent.name
+                    print '  children of parent: ' + ','.join([x.name for x in dummy.parent.children])
+                    print '  parent of children: ' + ','.join([x.name for x in dummy.children])
+
+                print '  nodes_at_child_rank: ' + ','.join([x.name for x in nodes_at_child_rank])
+
+                next_nodes |= nodes_at_child_rank
+                print '  next_nodes: ' + ','.join([x.name for x in next_nodes])
+                print ''
+
+            nodes_at_rank = next_nodes
+
+        # remove dummy nodes
+        self._remove_dummy_nodes(root)
 
         # save marked tree
-        root.write(output)
+        return root
--- tests/test_mark_ranks.py
+++ tests/test_mark_ranks.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+"""Test marking of taxonomic ranks"""  #!/usr/bin/env python
 
 __author__ = "Donovan Park"
 __copyright__ = "Copyright 2015, The tax2tree project"
@@ -31,17 +31,17 @@ class MarkRanksTests(TestCase):
         mr = MarkRanks()
 
         # test ultrametric tree (i.e., equal branch lengths)
-        tree = TreeNode.read(StringIO(u'((a:1,b:1):1,(d:1,e:1):1);'))
+        tree = TreeNode.read(StringIO(u'((a:1,b:1):1,(c:1,d:1):1);'))
         avg_dist = mr.average_dist_to_leaves(tree)
         self.assertAlmostEqual(avg_dist, 2.0)
 
         # test calculation under varying branch lengths
-        tree = TreeNode.read(StringIO(u'((a:2,b:2):2,(d:1,e:1):1);'))
+        tree = TreeNode.read(StringIO(u'((a:2,b:2):2,(c:1,d:1):1);'))
         avg_dist = mr.average_dist_to_leaves(tree)
         self.assertAlmostEqual(avg_dist, 3.0)
 
         # test with branch length of zero
-        tree = TreeNode.read(StringIO(u'((a:1,b:2):0,(d:2,e:1):1);'))
+        tree = TreeNode.read(StringIO(u'((a:1,b:2):0,(c:2,d:1):1);'))
         avg_dist = mr.average_dist_to_leaves(tree)
         self.assertAlmostEqual(avg_dist, 2.0)
 
@@ -53,9 +53,118 @@ class MarkRanksTests(TestCase):
         # test calculation on internal nodes
         for child in tree.children:
             avg_dist = mr.average_dist_to_leaves(child)
-            self.assertAlmostEqual(avg_dist, 6.0 / 4)
+            self.assertAlmostEqual(avg_dist, 3.0 / 2)
 
+    def test_mark_ultrametric(self):
+        """Test marking of taxonomic ranks on ultrametric tree"""
 
+        mr = MarkRanks()
+
+        tree = StringIO(u'((a:5,b:5)t1:1,(c:1,d:1)t2:5)root;')
+        root = mr.mark(tree, ['root'], 0)
+
+        # verify rank assignments of nodes
+        pass_test = True
+        try:
+            root.find('S__|a')
+            root.find('S__|b')
+            root.find('S__|c')
+            root.find('S__|d')
+            root.find('P__|t1')
+            root.find('S__|t2')
+            root.find('D__|root')
+        except:
+            pass_test = False
+
+        self.assertTrue(pass_test, "Unexpected taxonomic rank assigned to an internal node.")
+
+        self.assertEqual(root.count(), 7)
+        self.assertEqual(root.count(tips=True), 4)
+
+    def test_mark_skewed_tree(self):
+        """Test marking of taxonomic ranks on tree with highly varying rates of evolution"""
+
+        mr = MarkRanks()
+
+        tree = StringIO(u'((a:5,b:5)t1:1,(c:10,d:10)t2:5)root;')
+        root = mr.mark(tree, ['root'], 0)
+
+        # verify rank assignments of nodes
+        pass_test = True
+        try:
+            root.find('S__|a')
+            root.find('S__|b')
+            root.find('S__|c')
+            root.find('S__|d')
+            root.find('P__|t1')
+            root.find('O__|t2')
+            root.find('D__|root')
+        except:
+            pass_test = False
+
+        self.assertTrue(pass_test, "Unexpected taxonomic rank assigned to an internal node.")
+
+        self.assertEqual(root.count(), 7)
+        self.assertEqual(root.count(tips=True), 4)
+
+    def test_mark_skewed_leaves(self):
+        """Test marking of taxonomic ranks on tree with highly varying rates at leaf nodes"""
+
+        mr = MarkRanks()
+
+        tree = StringIO(u'((a:5,b:5)t1:1,(c:10,d:1)t2:5)root;')
+        root = mr.mark(tree, ['root'], 0)
+
+        # verify rank assignments of nodes
+        pass_test = True
+        try:
+            root.find('S__|a')
+            root.find('S__|b')
+            root.find('S__|c')
+            root.find('S__|d')
+            root.find('P__|t1')
+            root.find('F__|t2')
+            root.find('D__|root')
+        except:
+            pass_test = False
+
+        self.assertTrue(pass_test, "Unexpected taxonomic rank assigned to an internal node.")
+
+        self.assertEqual(root.count(), 7)
+        self.assertEqual(root.count(tips=True), 4)
+
+    def test_mark_missing_label(self):
+        """Test proper handling of missing start label"""
+
+        mr = MarkRanks()
+
+        # test missing label
+        tree = StringIO(u'((a:1,b:1):1,(c:1,d:1):1)root;')
+        root = mr.mark(tree, ['bad_label'], 0)
+        self.assertIsNone(root)
+
+        # test valid labels
+        tree = StringIO(u'((a:1,b:1)r1:1,(c:1,d:1)r2:1)root;')
+        root = mr.mark(tree, ['r1', 'r2'], 0)
+        self.assertIsNotNone(root)
+
+    def test_remove_dummy_nodes(self):
+        """Test removal of dummy nodes (nodes with a single child)."""
+
+        mr = MarkRanks()
+
+        root = TreeNode.read(StringIO(u'(((a:1,b:1)c:1)d:1,(e:1,f:1)g:1)r;'))
+        mr._remove_dummy_nodes(root)
+
+        # verify node d has been removed
+        try:
+            node = root.find('d')
+        except:
+            node = None
+
+        self.assertIsNone(node)
+        self.assertEqual(root.count(), 7)
+        self.assertEqual(root.count(tips=True), 4)
 
 
 if __name__ == '__main__':
